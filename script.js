b5a315057b68464a745bbf4fd6384b7862a11939
Imranullah Khan <129750188+ImranullahKhann@users.noreply.github.com>
2024-04-17 22:27:00 +0500

    Feat: playRound function added.

--- script.js
+++ script.js
@@ -17,5 +17,67 @@ function getComputerChoice() {
     }
 }
 
-console.log(getComputerChoice())
+function playRound(playerSelection, computerSelection) {
+    playerSelection = playerSelection.toLowerCase();
+    computerSelection = computerSelection.toLowerCase();
+    // Depending on the combination of the two selections, return a string that declares the winner
+    // if player chooses rock
+    if (playerSelection === "rock")
+    {
+        // if computer chooses rock
+        if (computerSelection === "rock")
+        {
+            return "Tie!";
+        }
+        // else if computer chooses paper
+        else if (computerSelection === "paper")
+        {
+            return "You lose! Paper beats Rock!";
+        }
+        // else if computer chooses scissor
+        else if (computerSelection === "scissor")
+        {
+            return "You win! Rock beats Scissor!";
+        }
+    }
+    // else if player chooses paper
+    else if (playerSelection === "paper")
+    {
+        // if computer chooses rock
+        if (computerSelection === "rock")
+        {
+            return "You win! Paper beats Rock!";
+        }
+        // else if computer chooses paper
+        else if (computerSelection === "paper")
+        {
+            return "Tie!";
+        }
+        // else if computer chooses scissor
+        else if (computerSelection === "scissor")
+        {
+            return "You lose! Scissor beats Paper!";
+        }
+    }
+    // else if player chooses scissor
+    else if (playerSelection === "scissor")
+    {
+        // if computer chooses rock
+        if (computerSelection === "rock")
+        {
+            return "You lose! Rock beats Scissor!";
+        }
+        // else if computer chooses paper
+        else if (computerSelection === "paper")
+        {
+            return "You win! Scissor beats Paper";
+        }
+        // else if computer chooses scissor
+        else if (computerSelection === "scissor")
+        {
+            return "Tie!";
+        }
+    }
+}
 
+console.log(playRound("RocK", getComputerChoice()));
